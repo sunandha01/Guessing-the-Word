1f15cc645db3ccfceea0db8f6cfacdfb41c7c0d0
Sunandha <sunandha190811@gmail.com>
2025-02-27 19:09:58 +0530

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,89 @@
+'''
+import random
+
+wordList = ["rizz", "ohio", "sigma", "tiktok", "skibidi"]
+
+word = random.choice(wordList)
+
+guessedWord = ['_'] * len(word)
+
+attempts = 10
+
+while attempts > 0:
+   
+    print("\nCurrent word: " + ' '.join(guessedWord))
+
+    guess = input("Guess a letter: ").lower()
+   
+    if guess in word:
+        for i in range(len(word)):
+            if word[i] == guess:
+                guessedWord[i] = guess
+        print("Great guess!")
+    else:
+        attempts -= 1
+        print("Wrong guess! Attempts left: " + str(attempts))
+    if '_' not in guessedWord:
+        print("\nCongratulations!! You guessed the word: " + word)
+        break
+else:
+    print("\nYou've run out of attempts! The word was: " + word)
+'''
+import random
+import string
+
+def get_hangman_stage(attempts):
+    stages = [
+        """\n  +---+\n  |   |\n      |\n      |\n      |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n      |\n      |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n  |   |\n      |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n /|   |\n      |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n /|\  |\n      |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n /|\  |\n /    |\n      |\n=========\n""",
+        """\n  +---+\n  |   |\n  O   |\n /|\  |\n / \  |\n      |\n=========\n"""
+    ]
+    return stages[len(stages) - attempts - 1]
+
+def hangman():
+    word_list = ["rizz", "ohio", "sigma", "tiktok", "skibidi"]
+    word = random.choice(word_list)
+    guessed_word = ['_'] * len(word)
+    attempts = 6
+    guessed_letters = set()
+    
+    print("\nWelcome to Hangman!")
+    while attempts > 0:
+        print(get_hangman_stage(attempts))
+        print("Current word: ", ' '.join(guessed_word))
+        print("Guessed letters: ", ', '.join(sorted(guessed_letters)))
+        
+        guess = input("Guess a letter: ").lower()
+        
+        if len(guess) != 1 or guess not in string.ascii_lowercase:
+            print("Invalid input. Please enter a single letter.")
+            continue
+        
+        if guess in guessed_letters:
+            print("You've already guessed that letter. Try again.")
+            continue
+        
+        guessed_letters.add(guess)
+        
+        if guess in word:
+            for i in range(len(word)):
+                if word[i] == guess:
+                    guessed_word[i] = guess
+            print("Great guess!")
+        else:
+            attempts -= 1
+            print(f"Wrong guess! Attempts left: {attempts}")
+        
+        if '_' not in guessed_word:
+            print("\nCongratulations!! You guessed the word: ", word.upper())
+            break
+    else:
+        print(get_hangman_stage(attempts))
+        print("\nYou've run out of attempts! The word was: ", word.upper())
+        
+if __name__ == "__main__":
+    hangman()
